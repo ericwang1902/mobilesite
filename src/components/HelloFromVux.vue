78d55cbcd9bc370ad4f21f0a044aaf0310adae9b
ericwang1902 <2248930513@qq.com>
2017-02-12 16:06:20 +0800

    新建项目

---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="http://okxwe70nc.bkt.clouddn.com/2017-02-07--12-49-02">
+      <h1> </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="Vux" value="Cool" is-link></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
