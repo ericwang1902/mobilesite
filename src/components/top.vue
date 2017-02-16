8db9378a7ab6b4848ee0b5502d1ccb28e35a606f
ericwang1902 <2248930513@qq.com>
2017-02-16 10:38:01 +0800

    修改top

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <x-header :left-options="{showBack: false}">
-            {{title}}
+        <x-header style="background-color: rgb(65, 181, 191)" :left-options="{showBack: false}">
+            <clocker style="font-size: 12px" :time="time1">%H小时 %M 分 %S 秒</clocker>
         </x-header>
     </div>
 </template>
@@ -16,11 +16,15 @@ export default {
     },
     data () {
         return {
-            title:"本轮下单将于15点结束"
+            title:"本轮下单将于15点结束",
+            time1: '2017-02-16 12:00'
         }
     }
 }
 
 </script>
 <style>
+.header{
+    
+}
 </style>
