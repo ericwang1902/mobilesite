860070c2aa758fd1a0d6c3f62d9909562bcf4644
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-03-07 13:45:15 +0800

    做地区选择器表单。

--- src/components/usercenter/addressform.vue
+++ src/components/usercenter/addressform.vue
@@ -4,6 +4,31 @@
     </div>
 </template>
 <script>
+import config from '../../config/config'
+
+export default {
+    data(){
+        return {
+            districtData:[]
+        }
+    },
+    created(){
+        this.getdistricts();
+    },
+    methods:{
+        getdistricts:function(){
+            console.log(config.mdistricts)
+            this.axios.get(config.mdistricts)
+                      .then(response=>{
+                          console.log(response.data)
+                      })
+                      .catch(function(err){
+                          console.log(err);
+                      })
+                    
+        }
+    }
+}
 
 </script>
 <style>
--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,8 @@
 var baseURL = "http://121.42.56.71:8085";
 
 var config = {
-    suite: baseURL + "/sysmanage/suite"//套餐接口
+    suite: baseURL + "/sysmanage/suite",//套餐接口
+    mdistricts: baseURL + "/sysmanage/mdistrict"//地区选择器的数据
 }
 
 export default {
