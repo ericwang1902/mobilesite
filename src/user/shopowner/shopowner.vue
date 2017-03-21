fd5c2415704421552a5455914e932f9c85a6eaf0
ericwang1902 <2248930513@qq.com>
2017-03-21 13:23:54 +0800

    基本完成店主订单列表的内容。

--- src/user/shopowner/shopowner.vue
+++ src/user/shopowner/shopowner.vue
@@ -8,9 +8,12 @@
 
         <scroller lock-x scrollbar-y height="-45px" ref="scroller">
             <div>
-                <div v-for="item in orderlist" v-if="item.ficorder.ficorderstate ==='1'">
+                <div v-for="item in orderlist" v-if="ifshow(item.ficorder.ficorderstate)">
                     <div style="border: .025rem solid #f5f5f5">
                         <div>{{item.ordernum}}</div>
+                        <div v-for="goodsitem in item.goodslist">
+                            <div>{{goodsitem.goods.goodsname}} x {{goodsitem.goodscount}}</div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -30,7 +33,8 @@
             return {
                 openid: '',//query中的openid
                 userinfo: {},//根据openid去获取到的user数据
-                orderlist: []
+                orderlist: [],
+                status:1//1是表示未取件，2是已取件，3是送达，对应ficorder的状态常量
             }
         },
         created() {
@@ -38,6 +42,16 @@
             this.getuserinfo();
         },
         methods: {
+            ifshow:function(itemstate){
+                if(this.status==3)
+                {
+                    return true;
+                } else
+                {
+                    return itemstate ==this.status;
+                }
+                
+            },
             //获取用户信息
             getuserinfo: function () {
                 this.axios.get(config.muser + "?openid=" + this.openid)
@@ -65,7 +79,8 @@
             },
             //tab响应函数
             handler(val) {
-
+                this.status = val;
+                this.getUngetshoporder();
             }
         }
 
