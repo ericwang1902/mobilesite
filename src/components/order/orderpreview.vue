3b4cd9d782d96cadb7916f8bb9185870e47be63d
ericwang1902 <2248930513@qq.com>
2017-03-24 17:24:54 +0800

    完成微信支付的调通。

--- src/components/order/orderpreview.vue
+++ src/components/order/orderpreview.vue
@@ -135,12 +135,13 @@ export default {
             this.axios.post(config.morder, this.orderdata)
                 .then((response) => {
                     var resultobj = response.data;
+             
                     
                     //index中已经wx.config，这里直接发起微信支付。
                     wx.chooseWXPay({
                         timestamp: resultobj.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                         nonceStr: resultobj.noncestr, // 支付签名随机串，不长于 32 位
-                        package: "prepay_id="+resultobj.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                        package: resultobj.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                         signType: resultobj.sintype, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
                         paySign: resultobj.paysign, // 支付签名
                         success: function (res) {
