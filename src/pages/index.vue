c343a9c736e88faa583389d3444379901467cea6
ericwang1902 <2248930513@qq.com>
2017-03-24 13:33:46 +0800

    微信jssdk的配置。

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -46,36 +46,27 @@
             this.$store.commit('setUserId', userid);//2.将userid存放在store中
             console.log("userid:" + this.$store.getters.getUserId);
 
-            //获取后台的jsapiticket
+            console.log(this.url);
+
+            //调用微信签名计算接口，在服务器端进行计算
             this.axios.get(config.mwxjsapiticketurl)
                 .then((response) => {
-                    console.log(response.data.jsapiticket);
-                    this.jsapiticket = response.data.jsapiticket;
-                    this.noncestr = Math.random().toString(36).substr(2, 15);
-                    this.timestamp = parseInt(new Date().getTime() / 1000) + '';
-                    this.url = '';//需要获取
-
-                    //计算签名---需要修改
-                    var signature1= utils.wxjssign(this.jsapiticket, this.noncestr, this.timestamp, this.url);
-
-                    // wx.config({
-                    //     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                    //     appId: 'wx94e3a09e4149b262', // 必填，公众号的唯一标识
-                    //     timestamp: this.timestamp, // 必填，生成签名的时间戳
-                    //     nonceStr: this.noncestr, // 必填，生成签名的随机串
-                    //     signature: signature1,// 必填，签名，见附录1
-                    //     jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                    // });
+                    var configobj = response.data;
+                    console.log(configobj);
+                    wx.config({
+                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: configobj.appid, // 必填，公众号的唯一标识
+                        timestamp: configobj.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: configobj.noncestr, // 必填，生成签名的随机串
+                        signature: configobj.signature,// 必填，签名，见附录1
+                        jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    });
 
 
                 })
                 .catch(function (err) {
                     console.log(err);
                 })
-
-
-
-
 
 
         },
