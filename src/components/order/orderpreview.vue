9fa2957d2c7b199f6abbd39a57d5e26929956b45
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-04-05 08:35:59 +0800

    修改，增加mobile

--- src/components/order/orderpreview.vue
+++ src/components/order/orderpreview.vue
@@ -60,7 +60,8 @@ export default {
                 district: '',
                 region: '',
                 address: '',
-                note: ''
+                note: '',
+                mobile:''
             }
         }
     },
