61b091c77cc9cd4dfcf102c1fa07267433543a98
ericwang1902 <2248930513@qq.com>
2017-04-06 17:48:45 +0800

    修改界面。

--- src/user/courier/courier.vue
+++ src/user/courier/courier.vue
@@ -4,14 +4,7 @@
             <tab-item selected @on-item-click="handler(1)">客户订单</tab-item>
             <tab-item @on-item-click="handler(2)">店铺订单</tab-item>
         </tab>
-        <tabbar>
-            <tabbar-item @on-item-click="barhandler(1)">
-                <span slot="label">当前批次</span>
-            </tabbar-item>
-            <tabbar-item show-dot @on-item-click="barhandler(2)">
-                <span slot="label">历史批次</span>
-            </tabbar-item>
-        </tabbar>
+
     </div>
 </template>
 <script>
@@ -35,21 +28,6 @@
                     case 2:
 
                         console.log(2)
-                        break;
-                    default:
-                        break;
-
-                }
-            },
-            barhandler(val){
-                switch (val) {
-                    case 1:
-
-                        console.log(111)
-                        break;
-                    case 2:
-
-                        console.log(222)
                         break;
                     default:
                         break;
