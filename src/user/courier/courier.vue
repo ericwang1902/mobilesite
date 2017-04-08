dd712e7f87de5a5f1c8aaf494deeb82c24ceafe3
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-04-08 21:10:26 +0800

    实践

--- src/user/courier/courier.vue
+++ src/user/courier/courier.vue
@@ -7,9 +7,9 @@
         <scroller lock-x scrollbar-y height="-45px" ref="scroller">
             <div>
                 <div v-for="item in ficorderlist">
-                    <div style="border: .025rem solid #f5f5f5">
+                    <div style="border: .025rem solid #f5f5f5" @click="onficclick">
                         <div>{{item.ficordernum}}</div>
-                        <div>{{item.ficorderstate}}</div>   
+                        <div>{{getstate(item)}}</div>
                     </div>
                 </div>
             </div>
@@ -23,11 +23,11 @@
     import { Tabbar, TabbarItem } from 'vux'
 
     export default {
-        data(){
+        data() {
             return {
-                ficorderlist:[],
-                regionid:'',
-                openid:''
+                ficorderlist: [],
+                regionid: '',
+                openid: ''
             }
         },
         components: {
@@ -35,11 +35,20 @@
             Tabbar, TabbarItem
         },
         created() {
-           this.regionid = this.$route.query.regionid;
-           this.openid = this.$route.query.openid;
-           this.getficorderbyregion();
+            this.regionid = this.$route.query.regionid;
+            this.openid = this.$route.query.openid;
+            this.getficorderbyregion();
         },
         methods: {
+            getstate(ficorder) {
+                if (ficorder.ficorderstate == '1') {
+                    return "未取件"
+                }else if(ficorder.ficorderstate == '2'){
+                    return "已取件"
+                }else if(ficorder.ficorderstate =='3'){
+                    return "已送达"
+                }
+            },
             //根据openid查询用户信息
             getuserinfo() {
                 this.axios.get(config.muser + "?openid=" + this.openid)
@@ -53,15 +62,15 @@
                     })
             },
             //根据regionid获取ficorder
-            getficorderbyregion(){   
-                this.axios.get(config.mficorder+"?openid=" + this.openid+"&regionid="+this.regionid)
-                          .then((response)=>{
-                              this.ficorderlist = response.data;
-                              
-                          })
-                          .catch(function(err){
-                              console.log(err);
-                          })
+            getficorderbyregion() {
+                this.axios.get(config.mficorder + "?openid=" + this.openid + "&regionid=" + this.regionid)
+                    .then((response) => {
+                        this.ficorderlist = response.data;
+
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    })
             },
             handler(val) {
                 switch (val) {
@@ -77,6 +86,9 @@
                         break;
 
                 }
+            },
+            onficclick(){
+                console.log("11");
             }
         }
     }
